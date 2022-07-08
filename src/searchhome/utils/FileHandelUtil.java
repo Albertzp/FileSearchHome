231e24593339439ba529db9bba1616f85356d94a
albertzp <albertzp23@outlook.com>
2022-07-08 16:42:12 +0800

    更新

--- src/searchhome/utils/FileHandelUtil.java
+++ src/searchhome/utils/FileHandelUtil.java
@@ -2,6 +2,9 @@ package searchhome.utils;
 
 import java.util.Arrays;
 
+/**
+ * 类型区分工具类
+ */
 public class FileHandelUtil {
     public static boolean isAvisave(String sufix){
         return Arrays.asList(new String[]{"pdf","txt","epub","azw3","mobi","torrent","zip","wav"}).contains(sufix);
